7867b9664429f6bdb64c89b9d9c3089f708cbdd1
kntt32 <153593861+kntt32@users.noreply.github.com>
2024-10-07 21:56:31 +0900

    メモ帳の作り直し

--- App/NotePad/main.c
+++ App/NotePad/main.c
@@ -17,43 +17,91 @@ const Graphic_Color ui_color = {0xf0, 0xf0, 0xf0};
 const uintn width = 600;
 const uintn height = 450;
 
-typedef struct {
-    ascii path[DefaultBuffSize];
-    uintn layerId;
+uintn layerId;
+ascii path[DefaultBuffSize] = "";
 
-    App_Syscall_TextBox_Data textBoxData;
-} NotePad;
-
-void NotePad_init(void);
-void respondMouse(Task_Message* message);
-void flush(void);
-uintn NotePad_open(ascii path[DefaultBuffSize]);
+App_Syscall_EditBox_Data editboxData = {0, 32, width, height-32, NULL, 0, 0, 0, 0, 0, 0, 1, 1};
 
 
-NotePad notePad;
+void flush() {
+    App_Syscall_DrawButton(layerId, 1, 1, 48-2, 32-2, "Save");
+    App_Syscall_DrawButton(layerId, 49, 1, 48-2, 32-2, "Help");
+
+    App_Syscall_EditBox_Draw(layerId, &editboxData);
+
+    return;
+}
+
 
 sintn main(ascii arg[32]) {
     if(arg[0] == '\0') {
-        App_Syscall_Alert("No File Input", sizeof("No File Input"));
+        App_Syscall_Alert("notepad: No File Input", sizeof("notepad: No File Input"));
         return 1;
     }
-    App_Syscall_ExitStdIo();
 
-    NotePad_init();
-    NotePad_open(arg);
+    for(uintn i=0; i<DefaultBuffSize; i++) {
+        path[i] = arg[i];
+        if(arg[i] == '\0') break;
+    }
+    path[DefaultBuffSize-1] = '\0';
+
+    File_DirectoryEntry dirEntry;
+    if(App_Syscall_GetDirEntryByPath(path, DefaultBuffSize, &dirEntry)) {
+        App_Syscall_Alert("notepad: Invalid Input", sizeof("notepad: Invalid Input"));
+        return 2;
+    }
+
+    if(App_Syscall_AllocPage((dirEntry.size*2 + 1 + 0xfff)>>12, (void**)&(editboxData.buff))) {
+        App_Syscall_Alert("notepad: Failed 1", sizeof("notepad: Failed 1"));
+        return 3;
+    }
+    editboxData.buffSize = ((dirEntry.size*2 + 1 + 0xfff)>>12)<<12;
+
+    if(App_Syscall_MMapFile(path, DefaultBuffSize, editboxData.buffSize, editboxData.buff)) {
+        App_Syscall_Alert("notepad: Failed 2", sizeof("notepad: Failed 2"));
+        return 4;
+    }
+    editboxData.buff[editboxData.buffSize-1] = '\0';
+
+    if(App_Syscall_NewWindow(&layerId, 100, 100, width, height, path)) {
+        return 5;
+    }
+
     flush();
 
-    Task_Message message;
     while(1) {
-        App_Syscall_TextBox(notePad.layerId, 0, 32, &(notePad.textBoxData));
-        App_Syscall_ReadMessage(&message);
+        App_Syscall_EditBox_Response(layerId, editboxData.x, editboxData.y, &editboxData);
+
+        Task_Message message;
+        App_Syscall_CheckMessage(&message);
+
         switch(message.type) {
             case Task_Message_CloseWindow:
-                App_Syscall_Exit(0);
+                return 0;
             case Task_Message_Quit:
-                App_Syscall_Exit(0);
+                return 0;
             case Task_Message_MouseLayerEvent:
-                respondMouse(&message);
+                if(message.data.MouseLayerEvent.leftButton) {
+                    //Save
+                    if(0 <= message.data.MouseLayerEvent.x && message.data.MouseLayerEvent.x < 48
+                        && 0 <= message.data.MouseLayerEvent.y && message.data.MouseLayerEvent.y < 32) {
+                        App_Syscall_DrawButton_Pushed(layerId, 1, 1, 48-2, 32-2, "Save");
+                        for(uintn i=0; i<editboxData.buffSize; i++) {
+                            if(editboxData.buff[i] == '\0') {
+                                App_Syscall_WriteFileFromMem(path, DefaultBuffSize, i+1, editboxData.buff);
+                            }
+                        }
+                        flush();
+                    }
+                    //Help
+                    if(48 <= message.data.MouseLayerEvent.x && message.data.MouseLayerEvent.x < 48+48
+                        && 0 <= message.data.MouseLayerEvent.y && message.data.MouseLayerEvent.y < 32) {
+                        App_Syscall_DrawButton_Pushed(layerId, 49, 1, 48-2, 32-2, "Help");
+                        //Do Something
+                        flush();
+                    }
+                }
+                break;
             default:
                 break;
         }
@@ -63,82 +111,4 @@ sintn main(ascii arg[32]) {
 }
 
 
-void NotePad_init(void) {
-    notePad.path[0] = '\0';
-    if(App_Syscall_NewWindow(&(notePad.layerId), 100, 100, width, height, "NotePad")) {
-        App_Syscall_Alert("Unknown Error", sizeof("Unknown Error"));
-        App_Syscall_Exit(-1);
-    }
-
-    notePad.textBoxData.buffSize = 0;
-    notePad.textBoxData.buff = NULL;
-    notePad.textBoxData.width = width;
-    notePad.textBoxData.height = height-32;
-    notePad.textBoxData.scroll = 0;
-    notePad.textBoxData.cursorX = 0;
-    notePad.textBoxData.cursorY = 0;
-
-    return;
-}
-
-
-uintn NotePad_open(ascii path[DefaultBuffSize]) {
-    File_DirectoryEntry dirEnt;
-
-    for(uintn i=0; i<DefaultBuffSize; i++) {
-        notePad.path[i] = path[i];
-        if(path[i] == '\0') break;
-    }
-
-    if(App_Syscall_GetDirEntryByPath(notePad.path, DefaultBuffSize, &dirEnt)) {
-        App_Syscall_Alert("File Not Found", sizeof("File Not Found"));
-        return 1;
-    }
-
-    App_Syscall_FreePages((notePad.textBoxData.buffSize + 0xfff)>>12, notePad.textBoxData.buff);
-    notePad.textBoxData.buffSize = 0;
-    notePad.textBoxData.buff = NULL;
-
-    if(App_Syscall_AllocPage((dirEnt.size + 0xfff)>>12, (void**)&(notePad.textBoxData.buff))) {
-        App_Syscall_Alert("Failed to Open", sizeof("Failed to Open"));
-        App_Syscall_Exit(-1);
-    }
-
-    notePad.textBoxData.buffSize = ((dirEnt.size + 1 + 0xfff)>>12)<<12;
-
-    if(App_Syscall_MMapFile(notePad.path, DefaultBuffSize, notePad.textBoxData.buffSize, notePad.textBoxData.buff)) {
-        App_Syscall_Alert("File Not Found", sizeof("File Not Found"));
-        return 2;
-    }
-    notePad.textBoxData.buff[dirEnt.size] = '\0';
-
-    return 0;
-}
-
-
-void respondMouse(Task_Message* message) {
-    //保存ボタン
-    if(0 <= message->data.MouseLayerEvent.x && message->data.MouseLayerEvent.x < 48
-        && 0 <= message->data.MouseLayerEvent.y && message->data.MouseLayerEvent.y < 32) {
-        App_Syscall_DrawButton_Pushed(notePad.layerId, 1, 1, 48-2, 32-2, "save");
-        //save
-        for(uintn i=0; i<notePad.textBoxData.buffSize; i++) {
-            if(notePad.textBoxData.buff[i] == '\0') {
-                App_Syscall_WriteFileFromMem(notePad.path, DefaultBuffSize, i+1, notePad.textBoxData.buff);
-                return;
-            }
-        }
-        App_Syscall_Alert("Failed", sizeof("Failed"));
-    }
-
-    return;
-}
-
-
-void flush(void) {
-    App_Syscall_DrawSquare(notePad.layerId, 0, 0, width, 32, gray);
-    App_Syscall_DrawButton(notePad.layerId, 1, 1, 48-2, 32-2, "save");
-
-    return;
-}
 
